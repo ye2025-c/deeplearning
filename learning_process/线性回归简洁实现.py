9c4705b5ea1bf4eb2caf96d33399f1200c4dde70
shen <3032845608@qq.com>
2026-05-26 19:23:05 +0800

    初步了解

--- learning_process/线性回归简洁实现.py
+++ learning_process/线性回归简洁实现.py
@@ -20,3 +20,34 @@ data_iter = load_array((features, labels), batch_size)
 
 print(next(iter(data_iter))) # 返回一个批次的数据
 
+
+#定义模型
+from torch import nn
+net = nn.Sequential(nn.Linear(2,1))
+
+# 初始化模型参数
+net[0].weight.data.normal_(0, 0.01)
+net[0].bias.data.fill_(0)
+
+# 定义损失函数
+loss = nn.MSELoss()
+
+# 定义优化算法
+trainer = torch.optim.SGD(net.parameters(), lr=0.03)
+
+#训练模型
+num_epochs = 3
+for epoch in range(num_epochs):
+    for X, y in data_iter:
+        l = loss(net(X), y)
+        trainer.zero_grad()
+        l.backward()
+        trainer.step()
+    l = loss(net(features), labels)
+    print(f'epoch {epoch + 1}, loss {l:f}')
+
+#bias和权重的估计误差
+w = net[0].weight.data
+print('w的估计误差: ', true_w - w.reshape(true_w.shape))
+b = net[0].bias.data
+print('b的估计误差: ', true_b - b)
